2f1d53fe81a22931396a66f8109c990c61a078f6
nkanysio-ios <13.nkanyiso@gmail.com>
2021-03-05 20:33:06 +0200

    initial app create

---
+++ Friends/Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Nkanyiso Hlela on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
